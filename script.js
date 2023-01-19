a09c5017077fb53e071e373aa58db36375cfe8df
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-01-19 13:30:31 -0600

    Add rps logic

---
+++ script.js
@@ -0,0 +1,19 @@
+var getComputerChoice = Math.random()
+    if (getComputerChoice <= 0.33){
+        getComputerChoice = "Rock"
+    }
+    else if (getComputerChoice <= 0.66){
+        getComputerChoice = "Paper"
+    }
+    if (getComputerChoice <= 1.0){
+        getComputerChoice = "Scissors"
+    }
+const computerSelection = getComputerChoice
+var playerSelection
+
+function playRound(playerSelection, getComputerChoice){
+
+
+}
+
+console.log(computerSelection)
