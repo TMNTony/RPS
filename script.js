b75b7d4443579750658427524da9c2e849488dbb
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-01-19 20:41:40 -0600

    Add counter

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerScore = 0
+let computerScore = 0
+
+
 const playerSelection = "Rock"
 var getComputerChoice = Math.random()
     if (getComputerChoice <= 0.33){
@@ -12,33 +16,41 @@ var getComputerChoice = Math.random()
 
 const computerSelection = getComputerChoice
 
+for (let i = 0; i < 5; i++) {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection){
-        return "It's a tie";}
+            return "It's a tie";}
     
     else if (playerSelection === "Rock") {
         if (computerSelection === "Paper") {
+            computerScore++
             return "You Lose";}
         else  {
+            playerScore++
             return "You Win!!";}
         }
 
     else if (playerSelection === "Paper") {
         if (computerSelection === "Scissors") {
+            computerScore++
             return "You Lose";}
-        else {
+        else  {
+            playerScore++
             return "You Win!!";}
         }
 
     else if (playerSelection === "Scissors") {
         if (computerSelection === "Rock"){
+            computerScore++
             return "You Lose";}
         else  {
+            playerScore++
             return "You Win!!";}
         }
     } 
-      
+} 
 
-    console.log(playerSelection)
-    console.log(computerSelection)
+
     console.log(playRound(playerSelection, computerSelection))
+    console.log(playerScore)
+    console.log(computerScore)
